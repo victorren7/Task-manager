46d6a10f863c2c3db8823e1ff8b7feb385da32ca
victor <31532226+victorren7@users.noreply.github.com>
2023-01-19 11:24:48 -0800

    connected to mongodb

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-console.log('Task Manager App')
+const express = require('express')
+const app = express()
+const tasks = require('./routes/tasks')
+const connectDB = require('./db/connect')
+require('dotenv').config()
+
+//middleware
+
+app.use(express.json())
+
+app.get('/hello', (req,res) => {
+  res.send('Task manager app')
+})
+
+app.use('/api/v1/tasks', tasks)
+
+const port = 5000
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI)
+    app.listen(port, console.log(`server is listining on port ${port}...`))
+  } catch (err) {
+    console.log(error)
+  }
+}
+
+start()
+
